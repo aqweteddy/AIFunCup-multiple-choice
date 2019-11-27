737d8f8e88e47c97bd0114399ecf34fd218260a9
aqweteddy <keddy940199@gmail.com>
2019-11-28 00:07:10 +0800

    req

--- roberta_hugging_face/fine_tune.sh
+++ roberta_hugging_face/fine_tune.sh
@@ -10,10 +10,10 @@ python ./run.py \
 --do_lower_case \
 --data_dir $FOLDIR \
 --learning_rate 5e-5 \
---num_train_epochs 3 \
+--num_train_epochs 4 \
 --max_seq_length 512 \
 --output_dir model \
---per_gpu_eval_batch_size=1 \
+--per_gpu_eval_batch_size=16 \
 --per_gpu_train_batch_size=16 \
 --gradient_accumulation_steps 2 \
 --overwrite_output
